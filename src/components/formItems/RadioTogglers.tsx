71653672c6e09aff2a958b4bf588fb60c341b501
ghostxd <naclzfaklz@gmail.com>
2024-01-29 03:16:43 +0300

    wip

---
+++ src/components/formItems/RadioTogglers.tsx
@@ -0,0 +1,28 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+interface RadioTogglersProps {
+	options: {
+		value: string
+		icon: IconProp
+		label: string
+	}[]
+}
+
+const RadioTogglers = ({ options }: RadioTogglersProps) => {
+	return (
+		<div className='radio-togglers shadow'>
+			{options.map(option => (
+				<label>
+					<input type='radio' name='bgType' value={option.value} />
+					<div>
+						<FontAwesomeIcon icon={option.icon} className='h-5 w-5' />
+						<span>{option.label}</span>
+					</div>
+				</label>
+			))}
+		</div>
+	)
+}
+
+export default RadioTogglers
--- src/components/forms/PageSettingsForm.tsx
+++ src/components/forms/PageSettingsForm.tsx
@@ -1,4 +1,8 @@
-import { Image, PaletteIcon } from 'lucide-react'
+import { authOptions } from '@/app/utils/authOptions'
+import { faImage, faPalette } from '@fortawesome/free-solid-svg-icons'
+import { getServerSession } from 'next-auth'
+import Image from 'next/image'
+import RadioTogglers from '../formItems/RadioTogglers'
 
 interface PageModel {
 	_id: string
@@ -9,29 +13,34 @@ interface PageModel {
 	__v: number
 }
 
-const PageSettingsForm = ({ page }: { page: PageModel }) => {
+const PageSettingsForm = async ({ page }: { page: PageModel }) => {
+	const session = await getServerSession(authOptions)
 	return (
 		<div className='-m-4'>
 			<form>
-				<div className='bg-gray-400 h-32 flex justify-center items-center rounded-t-md'>
-					<div className='radio-togglers shadow'>
-						<label>
-							<input type='radio' name='bgType' value='color' />
-							<div>
-								<PaletteIcon />
-								<span>Color</span>
-							</div>
-						</label>
-						<label>
-							<input type='radio' name='bgType' value='image' />
-							<div>
-								<Image />
-								<span>Image</span>
-							</div>
-						</label>
-					</div>
+				<div className='bg-gray-400 py-16 flex justify-center items-center rounded-t-md'>
+					<RadioTogglers
+						options={[
+							{ value: 'color', icon: faPalette, label: 'Color' },
+							{ value: 'image', icon: faImage, label: 'Image' },
+						]}
+					/>
 				</div>
-				<div>avatar</div>
+				<div className='flex justify-center'>
+					<Image
+						src={session?.user?.image || ''}
+						height={128}
+						width={128}
+						alt='avatar'
+						priority
+						className='rounded-full relative -top-8 border-4 border-gray-200 shadow-lg shadow-black/50'
+					/>
+				</div>
+				<div>
+					<input type='text' placeholder='Display name' />
+					<input type='text' placeholder='Location' />
+					<textarea name='' placeholder='Bio'></textarea>
+				</div>
 			</form>
 		</div>
 	)
